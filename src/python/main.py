fb9fa6203268d1fbc087b14259b8faa718e9c051
Masroor Khonkhodzhaev <66025948+masroor47@users.noreply.github.com>
2024-06-15 12:19:18 -0400

    simple test of matmul

--- src/python/main.py
+++ src/python/main.py
@@ -1,8 +1,8 @@
 import numpy as np
-from matrix_ops_wrapper import add_matrices
+from matrix_ops_wrapper import add_matrices, matmul_simple
 
 
-def main():
+def test_elementwise_add():
     rows = 1<<15
     cols = 1<<15
     rows = int(rows * 1.6)
@@ -16,6 +16,26 @@ def main():
     for _ in range(10):
         c = add_matrices(a, b)
     print(f"c in py main: \n{c}")
+
+def test_matmul_simple():
+    lrows = 1<<14
+    lcols = 1<<15
+    rrows = 1<<15
+    rcols = 1<<15
+
+    lshape = (lrows, lcols)
+    rshape = (rrows, rcols)
+    left = np.ones(lshape).astype(np.float32)
+    right = np.ones(rshape).astype(np.float32)
+
+    c = matmul_simple(left, right)
+
+    print(c)
+    print(c.shape)
+
+
+def main():
+    test_matmul_simple()
 
 if __name__ == '__main__':
     main()

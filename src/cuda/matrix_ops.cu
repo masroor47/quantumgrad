9476960785d245f700d0dcd82097cbdf75142cc1
Masroor Khonkhodzhaev <66025948+masroor47@users.noreply.github.com>
2024-06-15 12:18:57 -0400

    add matmul_simple

--- src/cuda/matrix_ops.cu
+++ src/cuda/matrix_ops.cu
@@ -10,7 +10,22 @@ __global__ void add_matrices(float *a, float *b, float *c, int rows, int cols) {
     }
 }
 
-extern "C" void matrix_add(float *a, float *b, float *c, int rows, int cols) {
+__global__ void matmul_simple_(float *left, float *right, float *result, int lrows, int lcols, int rrows, int rcols) {
+    // current row of left, current col of right
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < lrows && col < rcols) {
+        float temp_sum = 0.0;
+        // dot product
+        for (int i = 0; i < lcols; i++) {
+            temp_sum += left[row * lcols + i] * right[i * rcols + col];
+        }
+        result[row * rcols + col] = temp_sum;
+    }
+}
+
+extern "C" void add(float *a, float *b, float *c, int rows, int cols) {
     float *d_a, *d_b, *d_c;
 
     printf("Allocating %.2f GB of GPU memory\n", (double)(rows * cols * sizeof(float) * 3) / (1024 * 1024 * 1024));
@@ -38,4 +53,78 @@ extern "C" void matrix_add(float *a, float *b, float *c, int rows, int cols) {
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
+}
+
+extern "C" void matmul_simple(float *left, float *right, float *result, int lrows, int lcols, int rrows, int rcols) {
+    float *d_left, *d_right, *d_result;
+    size_t left_size = lrows * lcols * sizeof(float);
+    size_t right_size = rrows * rcols * sizeof(float);
+    size_t result_size = lrows * rcols * sizeof(float);
+
+    // printf("%f, %f, ... %f\n", left[0], left[1], left[lrows * lcols - 1]);
+    // printf("%f, %f, ... %f\n", right[0], right[1], right[rrows * rcols - 1]);
+
+    printf("Allocating %.2f GB of GPU memory\n", (double)((lrows * lcols + rrows * rcols + lrows * rcols)* sizeof(float)) / (1024 * 1024 * 1024));
+    cudaError_t err;
+    err = cudaMalloc((void**)&d_left, left_size);
+    if (err != cudaSuccess) {
+        printf("CUDA malloc failed for d_left: %s\n", cudaGetErrorString(err));
+        return;
+    }
+
+    err = cudaMalloc((void**)&d_right, right_size);
+    if (err != cudaSuccess) {
+        printf("CUDA malloc failed for d_right: %s\n", cudaGetErrorString(err));
+        cudaFree(d_left);
+        return;
+    }
+
+    err = cudaMalloc((void**)&d_result, result_size);
+    if (err != cudaSuccess) {
+        printf("CUDA malloc failed for d_result: %s\n", cudaGetErrorString(err));
+        cudaFree(d_left);
+        cudaFree(d_right);
+        return;
+    }
+
+    err = cudaMemcpy(d_left, left, left_size, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        printf("CUDA memcpy failed for d_left: %s\n", cudaGetErrorString(err));
+        cudaFree(d_left);
+        cudaFree(d_right);
+        cudaFree(d_result);
+        return;
+    }
+
+    err = cudaMemcpy(d_right, right, right_size, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        printf("CUDA memcpy failed for d_right: %s\n", cudaGetErrorString(err));
+        cudaFree(d_left);
+        cudaFree(d_right);
+        cudaFree(d_result);
+        return;
+    }
+
+    int BLOCK_SIZE = 16;
+    int GRID_SIZE_ROWS = (int)ceil((float)lrows / BLOCK_SIZE);
+    int GRID_SIZE_COLS = (int)ceil((float)rcols / BLOCK_SIZE);
+    
+    dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 gridSize(GRID_SIZE_COLS, GRID_SIZE_ROWS);
+
+    matmul_simple_<<<gridSize, blockSize>>>(d_left, d_right, d_result, lrows, lcols, rrows, rcols);
+
+    cudaError_t kernelErr = cudaGetLastError();
+    if (kernelErr != cudaSuccess) {
+        printf("Kernel launch failed: %s\n", cudaGetErrorString(kernelErr));
+    }
+
+    cudaMemcpy(result, d_result, result_size, cudaMemcpyDeviceToHost);
+
+    // printf("after kernel execution. result:\n");
+    // printf("%f, %f, ... %f\n", result[0], result[1], result[lrows * rcols - 1]);
+
+    cudaFree(d_left);
+    cudaFree(d_right);
+    cudaFree(d_result);
 }
--- src/python/matrix_ops_wrapper.py
+++ src/python/matrix_ops_wrapper.py
@@ -3,14 +3,22 @@ import numpy as np
 
 lib = ctypes.cdll.LoadLibrary('./lib/matrix_ops.so')
 
-lib.matrix_add.argtypes = [ctypes.POINTER(ctypes.c_float),
-                           ctypes.POINTER(ctypes.c_float),
-                           ctypes.POINTER(ctypes.c_float),
-                           ctypes.c_int,
-                           ctypes.c_int]
+lib.add.argtypes = [ctypes.POINTER(ctypes.c_float),
+                    ctypes.POINTER(ctypes.c_float),
+                    ctypes.POINTER(ctypes.c_float),
+                    ctypes.c_int,
+                    ctypes.c_int]
+
+lib.matmul_simple.argtypes = [ctypes.POINTER(ctypes.c_float),
+                              ctypes.POINTER(ctypes.c_float),
+                              ctypes.POINTER(ctypes.c_float),
+                              ctypes.c_int,
+                              ctypes.c_int,
+                              ctypes.c_int,
+                              ctypes.c_int]
 
 def add_matrices(a, b):
-    assert a.shape == b.shape, "Matrices must have the same shape"
+    assert a.shape == b.shape, "Matrices must have the same shape."
 
     rows, cols = a.shape
     c = np.zeros_like(a)
@@ -19,8 +27,24 @@ def add_matrices(a, b):
     b_ptr = b.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
     c_ptr = c.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
 
-    lib.matrix_add(a_ptr, b_ptr, c_ptr,
-                   ctypes.c_int(rows),
-                   ctypes.c_int(cols))
+    lib.add(a_ptr, b_ptr, c_ptr,
+            ctypes.c_int(rows),
+            ctypes.c_int(cols))
 
     return c
+
+def matmul_simple(left, right):
+    lrows, lcols = left.shape
+    rrows, rcols = right.shape
+    
+    assert lcols == rrows, "Inner dimension of matrices must be equal."
+
+    result = np.zeros((lrows, rcols)).astype(np.float32)
+
+    left_ptr = left.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    right_ptr = right.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    result_ptr = result.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+
+    lib.matmul_simple(left_ptr, right_ptr, result_ptr, lrows, lcols, rrows, rcols)
+
+    return result

c23f113cf284102dfc8e177dd993dd6eadb696f5
Masroor Khonkhodzhaev <66025948+masroor47@users.noreply.github.com>
2024-06-19 20:39:28 -0400

    add memory handlers

--- quantumgrad/src/python/cuda_wrapper/cuda_interface.py
+++ quantumgrad/src/python/cuda_wrapper/cuda_interface.py
@@ -3,15 +3,27 @@ import numpy as np
 
 import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
-lib_path = os.path.join(current_dir, '../../../lib/matrix_ops.so')
+lib_path = os.path.join(current_dir, '../../../../lib/matrix_ops.so')
 cuda_lib = ctypes.CDLL(lib_path)
 
+# Define the return types and argument types for the CUDA functions
+cuda_lib.allocate_gpu_memory.restype = ctypes.c_void_p
+cuda_lib.allocate_gpu_memory.argtypes = [ctypes.c_size_t]
+
+cuda_lib.free_gpu_memory.restype = None
+cuda_lib.free_gpu_memory.argtypes = [ctypes.c_void_p]
+
+cuda_lib.copy_to_gpu.restype = None
+cuda_lib.copy_to_gpu.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_size_t]
+
 def cpu_to_gpu(data):
+    print('cuda interface: moving cpu to gpu')
     gpu_data = cuda_lib.allocate_gpu_memory(data.size)
     cuda_lib.copy_to_gpu(data.ctypes.data, gpu_data, data.size)
     return gpu_data
 
 def gpu_to_cpu(gpu_data, shape):
+    print('cuda interface: moving gpu to cpu')
     cpu_data = np.empty(shape, dtype=np.float32)
     cuda_lib.copy_gpu_to_cpu(gpu_data, cpu_data.ctypes.data, cpu_data.size)
     return cpu_data

bba268de9cf50186e5584436e21bc2b4e3271fea
Masroor Khonkhodzhaev <66025948+masroor47@users.noreply.github.com>
2024-06-19 20:40:54 -0400

    create custom Module with two layers

--- notebooks/mnist.py
+++ notebooks/mnist.py
@@ -6,9 +6,35 @@ from quantumgrad.src.python.nn.nonlinearities import ReLU
 
 import numpy as np
 
+class BobNet(Module):
+    def __init__(self, in_nodes, out_nodes):
+        super(BobNet, self).__init__()
+        self.fc1 = Linear(in_nodes, 128)
+        self.fc2 = Linear(128, out_nodes)
+    
+    def forward(self, x):
+        x = ReLU(self.fc1(x))
+        return ReLU(self.fc2(x))
+
 if __name__ == '__main__':
-    linear = Linear(784, 10)
-    relu = ReLU()
-    output = relu(linear(np.random.randn(1, 784)))
-    print(output.shape)
-    print(output)
+    device = 'cuda'
+    # device = 'cpu'
+    bobNet = BobNet(784, 10).to(device)
+    print(f"data: {bobNet.fc1.weight._data}")
+    print(f"{len(bobNet.parameters()) = }")
+
+    # can't count all params if device is cuda
+    # total_params = sum(p.numel() for p in bobNet.parameters())
+    # print(f"{total_params = }")
+
+
+    import time
+    time.sleep(3)
+
+    bobNet = bobNet.to('cpu')
+
+    num_weights = 784 * 128 + 128 * 10
+    num_biases = 128 + 10
+    num_floats_in_model = num_weights + num_biases
+    megabytes_in_model = num_floats_in_model * 4 / 1024
+    print(f"you are using {megabytes_in_model:.2f}MB on device {device}")

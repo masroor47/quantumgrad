fd03d07b81a3ff9152a40a1f4ec8736b553f9aa3
Masroor Khonkhodzhaev <66025948+masroor47@users.noreply.github.com>
2024-06-20 23:14:39 -0400

    adapt Parameter class to use Tensor

--- quantumgrad/nn/parameter.py
+++ quantumgrad/nn/parameter.py
@@ -1,22 +1,10 @@
 import quantumgrad.cuda as cuda
+from quantumgrad.tensor import Tensor
 
-class Parameter:
-    def __init__(self, data):
-        self._data = data
-        self._device = 'cpu'
+class Parameter(Tensor):
+    def __init__(self, data, device='cpu'):
+        super().__init__(data, device=device, requires_grad=True)
 
-    def to(self, device):
-        if device == 'cuda':
-            self._data = cuda.cpu_to_gpu(self._data)
-        else:
-            self._data = cuda.gpu_to_cpu(self._data)
-        self._device = device
-        return self
-    
-    def numel(self):
-        if self._device == 'cpu':
-            return self._data.size
-        else:
-            # TODO: count param size if data is on cuda
-            print("device is cuda, can't give you the size rn")
-            raise NotImplementedError
+    def __repr__(self) -> str:
+        return f"Parameter data:\n{self.data}\n"
+        

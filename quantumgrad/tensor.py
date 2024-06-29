b3a76c81a87143dfd86e0186e3eaa52456fe321d
Masroor Khonkhodzhaev <66025948+masroor47@users.noreply.github.com>
2024-06-28 20:16:02 -0400

    add a bunch of things

--- quantumgrad/tensor.py
+++ quantumgrad/tensor.py
@@ -2,17 +2,34 @@ import numpy as np
 from . import cuda
 
 class Tensor:
-    def __init__(self, data, device='cpu', requires_grad=False):
+    def __init__(self, data, device='cpu', requires_grad=False, shape=None):
+        # print('Creating tensor')
+        assert device == 'cpu' or (device == 'cuda' and shape is not None), 'shape must be provided for cuda tensors'
         self._device = device
         self._requires_grad = requires_grad
         self._grad = None
+        # self._shape = data.shape if isinstance(data, np.ndarray) else None
 
         if device == 'cpu':
-            self._data = np.array(data)
+            if isinstance(data, np.ndarray):
+                self._data = data
+                self._shape = self._data.shape
+            else:
+                # print('I guess this is not an ndarray')
+                self._data = np.array(data)
+                self._shape = self._data.shape
         elif device == 'cuda':
-            self._data = cuda.allocate_device_memory(data)
+            self._data = data
+            # self._data = cuda.allocate_device_memory(data)
+            self._shape = shape
         else:
             raise ValueError(f"Unsupported device: {device}")
+        
+        print(f'Created tensor on {device} with shape {self._shape}')
+    
+    def __del__(self):
+        if self._device == 'cuda':
+            cuda.free_gpu_memory(self._data)
         
     @property
     def data(self):
@@ -30,17 +47,25 @@ class Tensor:
     def grad(self):
         return self._grad
 
+    @property
+    def shape(self):
+        return self._shape
+
     def to(self, device):
+        print(f"Moving tensor to {device}")
         if device == self._device:
             return self
         elif device == 'cpu':
-            data = cuda.gpu_to_cpu(self._data)
+            print(f"type of self._data: {type(self._data)}")
+            data = cuda.gpu_to_cpu(self._data, self._shape)
         elif device == 'cuda':
             data = cuda.cpu_to_gpu(self._data)
         else:
             raise ValueError(f"Unsupported device: {device}")
+        
+        # self._device = device
 
-        new_tensor = Tensor(data, device=device, requires_grad=self._requires_grad)
+        new_tensor = Tensor(data, device=device, requires_grad=self._requires_grad, shape=self._shape)
         if self._grad is not None:
             new_tensor._grad = self._grad.to(device)
         return new_tensor

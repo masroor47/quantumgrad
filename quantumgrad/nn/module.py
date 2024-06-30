7c62016334499eea854f9d2e839e26d00c237234
Masroor Khonkhodzhaev <66025948+masroor47@users.noreply.github.com>
2024-06-30 01:12:04 -0400

    getting somewhere

--- quantumgrad/nn/module.py
+++ quantumgrad/nn/module.py
@@ -12,25 +12,46 @@ class Module:
 
     def to(self, device):
         if self._device == device:
-            return self
+            return 
+
+        # print(f'\nModule; before moving to {device}\n')
+        # for param in self._parameters:
+        #     print(f"param.device: {param.device}; {param}")
+        
+        # print(f"weight device: {self.weight.device}; {self.weight}")
+        # print(f"bias device: {self.bias.device}; {self.bias}")
+        # print()
         
         for i, param in enumerate(self._parameters):
-            self._parameters[i] = param.to(device)
+            new_param = param.to(device)
+            self._parameters[i] = new_param
+            # Update corresponding attribute if it exists
+            for key, value in self.__dict__.items():
+                if value is param:
+                    # print(f'key: {key}; value: {value}; \nparam: {param}; \nnew_param: {new_param}')
+                    setattr(self, key, new_param)
+
+            print()
         
         for name, module in self.__dict__.items():
             if isinstance(module, Module):
+                print("moving submodules to device")
+                print(f"{name = };  {module = }")
                 setattr(self, name, module.to(device))
 
         # print device of every parameter
-        print()
-        for param in self._parameters:
-            print(f"param.device: {param.device}")
-        print()
+        # print()
+        # for param in self._parameters:
+        #     print(f"param.device: {param.device}; {param}")
+        
+        # print(f"weight device: {self.weight.device}; {self.weight}")
+        # print(f"bias device: {self.bias.device}; {self.bias}")
+        # print()
 
 
         self._device = device
         print(f"Moved module to {self._device}")
-        return self
+        # return self
 
     def parameters(self):
         params = self._parameters[:]

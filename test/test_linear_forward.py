168f587e2199cb5db82fa920b0b8c65bd4962a34
Masroor Khonkhodzhaev <66025948+masroor47@users.noreply.github.com>
2024-07-04 17:56:27 -0400

    batch input test

--- test/test_linear_forward.py
+++ test/test_linear_forward.py
@@ -28,6 +28,25 @@ class TestLinearForward(unittest.TestCase):
         tensor_input = Tensor(np_input).to('cuda')
         tensor_output = linear_layer(tensor_input).to('cpu')
         np_output = np.dot(np_input, cpu_weights_data.T) + cpu_bias_data
+        np.testing.assert_allclose(tensor_output.data[:, 0], np_output, rtol=1e-5, atol=1e-5)
+    
+    def test_linear_forward_batch_input_cuda(self):
+        input_dim = 200
+        output_dim = 100
+        batch_size = 16
+        linear_layer = nn.Linear(input_dim, output_dim)
+        cpu_weights_data = linear_layer.weight.data.copy()
+        cpu_bias_data = linear_layer.bias.data.copy()
+        linear_layer.to('cuda')
+        self.assertEqual(linear_layer.weight.device, 'cuda')
+        np_input = np.random.rand(input_dim, batch_size).astype(np.float32)
+        tensor_input = Tensor(np_input).to('cuda')
+        tensor_output = linear_layer(tensor_input).to('cpu')
+        cpu_bias_data_expanded = np.tile(cpu_bias_data[:, np.newaxis], (1, batch_size))
+        print(f"cpu_bias_data_expanded: {cpu_bias_data_expanded.shape}")
+        np_output = np.dot(cpu_weights_data, np_input) + cpu_bias_data_expanded
+        print(f"np_output: {np_output.shape}")
+        print(f"tensor_output: {tensor_output.shape}")
         np.testing.assert_allclose(tensor_output.data, np_output, rtol=1e-5, atol=1e-5)
 
 

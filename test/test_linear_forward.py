b466ed20b8954a3c76d9d9c8b36d32306ce1561d
Masroor Khonkhodzhaev <66025948+masroor47@users.noreply.github.com>
2024-07-12 21:09:32 -0400

    fix spelling

--- test/test_linear_forward.py
+++ test/test_linear_forward.py
@@ -32,8 +32,8 @@ class TestLinearForward(unittest.TestCase):
     def test_linear_forward_cuda(self):
         linear_layer = nn.Linear(self.input_dim, self.output_dim)
         cpu_weights_data = linear_layer.weight.data.copy()
-        cpu_nonzeor_bias = np.random.rand(self.output_dim).astype(np.float32)
-        linear_layer.bias._data = cpu_nonzeor_bias
+        cpu_nonzero_bias = np.random.rand(self.output_dim).astype(np.float32)
+        linear_layer.bias._data = cpu_nonzero_bias
 
         linear_layer.to('cuda')
         self.assertEqual(linear_layer.weight.device, 'cuda')
@@ -42,15 +42,15 @@ class TestLinearForward(unittest.TestCase):
         tensor_input = Tensor(np_input).to('cuda')
 
         tensor_output = linear_layer(tensor_input).to('cpu')
-        np_output = np.dot(np_input, cpu_weights_data.T) + cpu_nonzeor_bias
+        np_output = np.dot(np_input, cpu_weights_data.T) + cpu_nonzero_bias
 
         np.testing.assert_allclose(tensor_output.data[:, 0], np_output, rtol=1e-5, atol=1e-5)
     
     def test_linear_forward_batch_input_cuda(self):
         linear_layer = nn.Linear(self.input_dim, self.output_dim)
         cpu_weights_data = linear_layer.weight.data.copy()
-        cpu_nonzeor_bias = np.random.rand(self.output_dim).astype(np.float32)
-        linear_layer.bias._data = cpu_nonzeor_bias
+        cpu_nonzero_bias = np.random.rand(self.output_dim).astype(np.float32)
+        linear_layer.bias._data = cpu_nonzero_bias
 
         linear_layer.to('cuda')
         self.assertEqual(linear_layer.weight.device, 'cuda')
@@ -59,7 +59,7 @@ class TestLinearForward(unittest.TestCase):
         tensor_input = Tensor(np_input).to('cuda')
 
         tensor_output = linear_layer(tensor_input).to('cpu')
-        np_output = np.dot(cpu_weights_data, np_input) + cpu_nonzeor_bias[:, np.newaxis]
+        np_output = np.dot(cpu_weights_data, np_input) + cpu_nonzero_bias[:, np.newaxis]
 
         np.testing.assert_allclose(tensor_output.data, np_output, rtol=1e-5, atol=1e-5)
     
@@ -79,39 +79,41 @@ class TestLinearForward(unittest.TestCase):
         np_result = np.maximum(0, np.dot(linear_layer.weight._data, x) + linear_layer.bias._data[:, np.newaxis])
         np.testing.assert_allclose(y, np_result, rtol=1e-5, atol=1e-5)
 
-    @unittest.skip("Skip CUDA tests for now")
+    
     def test_linear_forward_relu_cuda(self):
         linear_layer = nn.Linear(self.input_dim, self.output_dim)
         cpu_weights_data = linear_layer.weight.data.copy()
-        cpu_nonzeor_bias = np.random.rand(self.output_dim).astype(np.float32)
-        linear_layer.bias._data = cpu_nonzeor_bias
+        cpu_nonzero_bias = np.random.rand(self.output_dim).astype(np.float32)
+        linear_layer.bias._data = cpu_nonzero_bias
 
         linear_layer.to('cuda')
         self.assertEqual(linear_layer.weight.device, 'cuda')
 
         np_input = np.random.rand(self.input_dim).astype(np.float32)
         tensor_input = Tensor(np_input).to('cuda')
-
-        tensor_output = linear_layer.forward_relu(tensor_input).to('cpu')
-        np_output = np.maximum(0, np.dot(np_input, cpu_weights_data.T) + cpu_nonzeor_bias)
+        
+        tensor_output = linear_layer.forward_relu(tensor_input)
+        tensor_output = tensor_output.to('cpu')
+        
+        np_output = np.maximum(0, np.dot(np_input, cpu_weights_data.T) + cpu_nonzero_bias)
 
         np.testing.assert_allclose(tensor_output.data[:, 0], np_output, rtol=1e-5, atol=1e-5)
 
-    @unittest.skip("Skip CUDA tests for now")
+    # @unittest.skip("Skip CUDA tests for now")
     def test_linear_forward_relu_batch_input_cuda(self):
         linear_layer = nn.Linear(self.input_dim, self.output_dim)
         cpu_weights_data = linear_layer.weight.data.copy()
-        cpu_nonzeor_bias = np.random.rand(self.output_dim).astype(np.float32)
-        linear_layer.bias._data = cpu_nonzeor_bias
+        cpu_nonzero_bias = np.random.rand(self.output_dim).astype(np.float32)
+        linear_layer.bias._data = cpu_nonzero_bias
 
         linear_layer.to('cuda')
         self.assertEqual(linear_layer.weight.device, 'cuda')
 
         np_input = np.random.rand(self.input_dim, self.batch_size).astype(np.float32)
         tensor_input = Tensor(np_input).to('cuda')
-
+        
         tensor_output = linear_layer.forward_relu(tensor_input).to('cpu')
-        np_output = np.maximum(0, np.dot(cpu_weights_data, np_input) + cpu_nonzeor_bias[:, np.newaxis])
+        np_output = np.maximum(0, np.dot(cpu_weights_data, np_input) + cpu_nonzero_bias[:, np.newaxis])
 
         np.testing.assert_allclose(tensor_output.data, np_output, rtol=1e-5, atol=1e-5)
 
